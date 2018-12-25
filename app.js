5b79895c6d7e801bb7fda6ee9de3acf61ef43520
Cakra Danu Sedayu <cakra.ds@gmail.com>
2018-12-25 11:43:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -81,16 +81,4 @@ app.post('/', (req, response) => {
     }
 });
 
-router.get('/me', function(req, res) {
-
-    let token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token, config.secret, (err, decoded)=> {
-      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-      
-      res.status(200).send(decoded);
-    });
-  });
-
 module.exports = app;
